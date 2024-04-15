e590c690a15fff8ed9b3b07c0fbc675d69882f59
MPC139 <mpormi@frba.utn.edu.ar>
2024-04-15 13:43:46 -0300

    Realice lo pedido en el TP0

---
+++ 00-CHelloWorld/hello.c
@@ -0,0 +1,9 @@
+#include "hello.h"
+
+
+
+int main()
+{
+    Saludar();
+    return 0;
+}
---
+++ 00-CHelloWorld/hello.h
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <string.h>
+
+void Saludar(void){
+    printf("Hello World!\n");
+}
